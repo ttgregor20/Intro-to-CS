1025c721894ae12f08b38c8032d06da135081df7
Tate Gregor20 <ttgregor20@AIR-WX0DP-3.local>
2019-02-27 14:04:45 -0600

    AddLogIn

---
+++ LoginScreen/LoginScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by Tate Gregor20 on 2/22/19.
+//  Copyright © 2019 Intro ComS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
